14bcbdb0eead9fd72bf311c2e4df6e6a15b26d59
Phillip Jensen <phillip@golemgrid.com>
2024-04-18 12:22:33 +0200

    Update api.py

--- docker-stack/golem-reputation-backend/reputation-backend/api2/api.py
+++ docker-stack/golem-reputation-backend/reputation-backend/api2/api.py
@@ -30,4 +30,54 @@ def list_provider_scores(request, network: str = 'polygon'):
         return JsonResponse({"error": "Data not available"}, status=503)
 
 
+from api.models import Provider, CpuBenchmark, NodeStatusHistory, TaskCompletion
+from api.scoring import calculate_uptime, penalty_weight
+from django.db.models import Subquery, OuterRef
 
+from django.db.models import Count, Case, When, FloatField
+from django.db.models.functions import Cast
+from django.db.models import Q
+
+@api.get("/filter")
+def filter_providers(request, uptime: float = None, cpuMultiThreadScore: float = None, cpuSingleThreadScore: float = None, successRate: float = None):
+    online_providers = Provider.objects.annotate(
+        latest_status=Subquery(
+            NodeStatusHistory.objects.filter(
+                provider=OuterRef('pk')
+            ).order_by('-timestamp').values('is_online')[:1]
+        )
+    ).filter(latest_status=True)
+    
+    if uptime is not None:
+        online_providers = online_providers.filter(
+            node_id__in=[p.node_id for p in online_providers if calculate_uptime(p.node_id) >= uptime])
+
+    if cpuMultiThreadScore is not None:
+        online_providers = online_providers.annotate(latest_cpu_multi_thread_score=Subquery(
+            CpuBenchmark.objects.filter(
+                provider=OuterRef('pk'),
+                benchmark_name="CPU Multi-thread Benchmark"
+            ).order_by('-created_at').values('events_per_second')[:1]
+        )).filter(latest_cpu_multi_thread_score__gte=cpuMultiThreadScore)
+    
+
+    if cpuSingleThreadScore is not None:
+        online_providers = online_providers.annotate(latest_cpu_single_thread_score=Subquery(
+            CpuBenchmark.objects.filter(
+                provider=OuterRef('pk'),
+                benchmark_name="CPU Single-thread Benchmark"
+            ).order_by('-created_at').values('events_per_second')[:1]
+        )).filter(latest_cpu_single_thread_score__gte=cpuSingleThreadScore)
+
+    if successRate is not None:
+        online_providers = online_providers.annotate(
+            successful_tasks=Count('taskcompletion', filter=Q(taskcompletion__is_successful=True)),
+            total_tasks=Count('taskcompletion'),
+            calculated_success_rate=Case(
+                When(total_tasks=0, then=None),
+                default=(Cast('successful_tasks', FloatField()) / Cast('total_tasks', FloatField()) * 100)
+            )
+        ).filter(calculated_success_rate__gte=successRate)
+
+    provider_ids = online_providers.values_list('node_id', flat=True)
+    return {"provider_ids": list(provider_ids)}
