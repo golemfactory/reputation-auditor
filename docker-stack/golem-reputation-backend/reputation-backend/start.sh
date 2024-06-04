0dccf30614d9f1451a2998cb86424c9f6ddd86a2
Phillip Jensen <phillip@golemgrid.com>
2024-06-04 09:39:45 +0200

    Fix deployment of gpu worker

--- docker-stack/golem-reputation-backend/reputation-backend/start.sh
+++ docker-stack/golem-reputation-backend/reputation-backend/start.sh
@@ -4,13 +4,13 @@ _cputype="$(uname -m)"
 case "$_cputype" in
 x86_64 | x86-64 | x64 | amd64)
     _cputype=x86_64
-    mv /yagna/amd64/yagna /root/.local/bin/yagna
-    mv /yagna/amd64/gftp /root/.local/bin/gftp
+    cp /yagna/amd64/yagna /root/.local/bin/yagna
+    cp /yagna/amd64/gftp /root/.local/bin/gftp
     ;;
 arm64 | aarch64)
     _cputype=aarch64
-    mv /yagna/arm64/yagna /root/.local/bin/yagna
-    mv /yagna/arm64/gftp /root/.local/bin/gftp
+    cp /yagna/arm64/yagna /root/.local/bin/yagna
+    cp /yagna/arm64/gftp /root/.local/bin/gftp
     ;;
 *)
     err "invalid cputype: $_cputype"
